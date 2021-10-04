0fac4aae2cb67d766be7fe1905c0a4d4b0f755cb
sheetalgcin <sheetalgcin@gmail.com>
2021-10-04 23:22:07 +0530

    first file of test

---
+++ spaceCnt.py
@@ -0,0 +1,13 @@
+'''write a code to find  more than one space in a row between words from a string '''
+
+str4=input("Enter a string to find the frequency occurence of characters")
+cnt = 1
+space = " "
+i = 0
+
+for i in range (len(str4)):
+    if str4[i]  == space and str4[i+1] == space :
+        cnt = cnt + 1
+        pos = i
+        if cnt > 1:
+            print("the entered string has more than one space in a row")
