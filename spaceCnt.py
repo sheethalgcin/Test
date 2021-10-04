3dd2ed8811c8dc16c840125b7cd19917f3704003
sheetalgcin <sheetalgcin@gmail.com>
2021-10-05 00:07:46 +0530

    hdsjh

--- spaceCnt.py
+++ spaceCnt.py
@@ -13,6 +13,10 @@ for i in range (len(str4)):
             print("the entered string has more than one space in a row")
         else:
             print("No space")
+<<<<<<< HEAD
         else:
 	        print("No space")
 
+=======
+            print('hello')
+>>>>>>> parent of 7375fe8 (hjd)
