c0802421893e68bb6ed6214654176f9df89fd067
sheetalgcin <sheetalgcin@gmail.com>
2021-10-05 11:33:05 +0530

    only foir thirsty branch

---
+++ exceptions.py
@@ -0,0 +1,32 @@
+print('Hello...')
+L= [10,20,30,40,50,60]
+
+try:
+    x = int(input('enter the first number'))
+    y = int(input('enter the second number'))
+    s = int(10)
+    s = 10 + 'abc'
+
+ #   import sheetal
+    q = x/y
+    print(q)
+    print(L[1])
+
+
+except ValueError:
+    print('I got value error')
+    y = int(input('enter the second number'))
+except ZeroDivisionError:
+    print('I got ZeroDivisionError')
+except IndexError:
+    print("I got index error")
+except TypeError:
+    print("I got Type error exception")
+except Exception:
+    print("I got exception")
+else:
+    print("i got no exception")
+finally:
+    print('I do always')
+
+print("end of app")
