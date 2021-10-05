a4f0eebd6a8416fffc59ba1b8473c3c7c0ad90ed
sheetalgcin <sheetalgcin@gmail.com>
2021-10-05 12:48:05 +0530

    dictOperations.py

---
+++ dictOperations.py
@@ -0,0 +1,14 @@
+d = { 'M' : 'Monday',  'F' : 'Friday', 'S' : 'Sunday' }
+print(d)
+
+print(d['F'])
+d['W'] = "Wednesday"
+print(d)
+d [ 'T' ] = 'Tuesday'
+print(d)
+k = d.keys()
+print(k)
+v = d.values()
+print(v)
+i = d.items()
+print(i)
